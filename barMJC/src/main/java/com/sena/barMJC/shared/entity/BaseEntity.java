b185ce5ee3ed3520be186f5ec3f9ad1e9d904fa9
JuanCortes2045 <juanestebanp171@gmail.com>
2026-06-11 17:25:43 -0500

    feat: correction to package names

--- barMJC/src/main/java/com/sena/barMJC/shared/entity/BaseEntity.java
+++ barMJC/src/main/java/com/sena/barMJC/shared/entity/BaseEntity.java
@@ -1,4 +1,39 @@
-package main.java.com.sena.barMJC.shared.entity;
+package com.sena.barMJC.shared.entity;
 
-public class BaseEntity {
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import lombok.Getter;
+import lombok.Setter;
+
+import  java.time.LocalDateTime;
+
+@Getter
+@Setter
+@MappedSuperclass
+public abstract class BaseEntity {
+
+    @Column(name="status", nullable= false)
+    protected Boolean status;
+
+    @Column(name="created_at", updatable= false, nullable= false)
+    protected LocalDateTime createdAt;
+
+    @Column(name="updated_at", nullable= false)
+    protected LocalDateTime updatedAt;
+
+
+    @PrePersist
+    protected  void onCreate(){
+        this.createdAt= LocalDateTime.now();
+        this.updatedAt= LocalDateTime.now();
+        this.status= true;
+    }
+
+    @PreUpdate
+    protected  void onUpdate(){
+        this.updatedAt= LocalDateTime.now();
+    }
+
 }
